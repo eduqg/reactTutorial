345f11d4c30d7965c06b9322c0be54ad459a6b1c
eduqg <eduardoqgomes@gmail.com>
2018-08-21 08:56:45 -0300

    initial commit

---
+++ my-app/tutorialCodes/extractMethod.js
@@ -0,0 +1,71 @@
+function formatDate(date) {
+    return date.toLocaleDateString();
+  }
+  
+  function Name(props) {
+    return (
+      <div className="UserInfo-name">
+        {props.user.name}
+      </div>
+    );
+  }
+  
+  function Avatar(props) {
+    return (
+      <img className="Avatar"
+        src={props.user.avatarUrl}
+        alt={props.user.name}
+      />
+    );
+  }
+  
+  function MessageReact(props) {
+    return (
+      <div className="Comment-text">{props.message}</div>
+    );
+  }
+  
+  function UserInfo(props) {
+    return (
+      <div className="UserInfo">
+        <Avatar user={props.user} />
+        <Name user={props.user} />
+      </div>
+      
+    );
+  }
+  
+  function Data(props) {
+    return (
+      <div className="Comment-date">
+          {formatDate(props.thisDate)}
+        </div>
+    );
+  }
+  
+  function Comment(props) {
+    return (
+      <div className="Comment">
+        <UserInfo user={props.author} />
+        <MessageReact message={props.text} />
+        <Data thisDate={props.date} />
+      </div>
+    );
+  }
+  
+  const comment = {
+    date: new Date(),
+    text: 'I hope you enjoy learning React!',
+    author: {
+      name: 'Hello Kitty',
+      avatarUrl: 'http://placekitten.com/g/64/64',
+    },
+  };
+  ReactDOM.render(
+    <Comment
+      date={comment.date}
+      text={comment.text}
+      author={comment.author}
+    />,
+    document.getElementById('root')
+  );
